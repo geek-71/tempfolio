496a1175076f5d841d8552318e9c1f254c10ff10
minhajalamdevcode <myminhajalam392@gmail.com>
2025-05-08 16:41:14 +0530

    add h1 element

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function App() {
   return (
-    <div className='bg-red-500 p-4'>Tempfolio</div>
+    <div className='bg-red-500 p-4'>Tempfolioeryyuruyt8eryud
+    <h1>jfhdkjdfhidf</h1>
+    </div>
   )
 }
 
